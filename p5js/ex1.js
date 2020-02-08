b629bc0402da0a04b5a01adff7935b39718efce9
okgeraldo <okgeraldo@users.noreply.github.com>
2020-02-08 01:25:11 +0000

    fill letters

--- p5js/ex1.js
+++ p5js/ex1.js
@@ -8,8 +8,9 @@ function setup(){
 
 function draw() {
   background(0);
+  fill(255);
   strokeWeight(2);
-
+fill(255);
   line(width/2, height/2, mouseX, mouseY);
   line(width/2, height/2,mouseX,height/2);
   line(mouseX, mouseY,mouseX,height/2);
