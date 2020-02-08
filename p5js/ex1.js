e9108b1c662167627d11f830b81ae1185a240b12
okgeraldo <okgeraldo@users.noreply.github.com>
2020-02-08 02:03:48 +0000

    color & stroke

--- p5js/ex1.js
+++ p5js/ex1.js
@@ -9,13 +9,16 @@ function setup(){
 function draw() {
   background(0);
   fill(255);
-  strokeWeight(2);
-  fill(255);
-  line(width/2, height/2, mouseX, mouseY);
+  stroke(207, 252, 3);
+  textSize(width / 50);
+  text(dist(width/2, height/2, mouseX, mouseY),50,50);
+  stroke(207, 252, 3);
+  strokeWeight(30);
+  fill(hue,207, 252, 3);
+  line(windowWidth, height/2,0, height/2);
   line(width/2, height/2,mouseX,height/2);
   line(mouseX, mouseY,mouseX,height/2);
 
-  textSize(width / 50);
-  text(dist(width/2, height/2, mouseX, mouseY),50,50);
+
 
 }
