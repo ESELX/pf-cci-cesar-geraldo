80c3dcdad36e0a6dd8d1d1aaf3fe73d2118bb312
okgeraldo <okgeraldo@users.noreply.github.com>
2020-02-09 10:50:15 +0000

    form

---
+++ p5js/ex10.js
@@ -0,0 +1,24 @@
+let mode
+
+function setup() {
+createCanvas(windowWidth,windowHeight);
+mode=1;
+
+}
+
+function draw(){
+  if (mode==1){
+    background (0, 29, 77);
+  }
+
+else if (mode == 2) {
+
+}
+else if (mode == 3) {
+
+}
+else {
+  background (0,0,255)
+}
+
+}
